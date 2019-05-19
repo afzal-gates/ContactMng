df3de220f766969f29ecceb7a7768c7ca194dbd1
afzal_gates <afzal.gates@gmail.com>
2019-05-19 16:04:38 +0600

    UnitTest

---
+++ ContactManagement.Test/UnitTestContactsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ContactManagement.Web.Controllers;
+using ContactManagement.Model;
+using System.Collections.Generic;
+
+namespace ContactManagement.Test
+{
+    [TestClass]
+    public class UnitTestContactsController
+    {
+        [TestMethod]
+        public void getContacts()
+        {
+            var list = GetContacts();
+            var controller = new ContactsController();
+            var result = controller.GetContacts("") as List<ContactModels>;
+            Assert.AreEqual(list.Count, result.Count);
+        }
+
+        public List<ContactModels> GetContacts()
+        {
+            var List = new List<ContactModels>();
+            List.Add(new ContactModels { ContactId = 1, Name = "Afzal", Email = "", MobileNo = "", Address = "", CategoryId = 1, ProfilePicture = "", UserName = "AASDD" });
+            List.Add(new ContactModels { ContactId = 2, Name = "Hossain", Email = "", MobileNo = "", Address = "", CategoryId = 2, ProfilePicture = "", UserName = "AASDD" });
+            List.Add(new ContactModels { ContactId = 3, Name = "Noman", Email = "", MobileNo = "", Address = "", CategoryId = 3, ProfilePicture = "", UserName = "AASDD" });
+
+            return List;
+        }
+    }
+}
